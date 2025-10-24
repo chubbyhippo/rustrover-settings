8ff897cd5f0604fb956cffca21d96c5f82425dcd
chubbyhippo <mkmsacc@outlook.com>
2025-10-24 22:35:57 +0700

    Update plugin list in `install-plugins.sh`.

--- install-plugins.sh
+++ install-plugins.sh
@@ -8,8 +8,14 @@ fi
 
 $cmd installPlugins \
 	IdeaVIM \
+	StringToolsPlugin \
+	com.fwdekker.randomness \
 	com.github.camork.fileExpander \
 	com.joshestein.ideavim-quickscope \
-	com.julienphalip.ideavim.functiontextobj \
 	com.julienphalip.ideavim.peekaboo \
-	eu.theblob42.idea.whichkey
+	com.jetbrains.kmm \
+	com.joshestein.ideavim-quickscope \
+	eu.theblob42.idea.whichkey \
+	indent-rainbow.indent-rainbow \
+	izhangzhihao.rainbow.brackets.lite \
+	org.sonarlint.idea
