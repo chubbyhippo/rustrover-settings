49398445e20d15434d050207e05d168552f92acf
chubbyhippo <mkmsacc@outlook.com>
2025-10-24 22:33:44 +0700

    Normalize indentation in `install-plugins.sh`.

--- install-plugins.sh
+++ install-plugins.sh
@@ -3,13 +3,13 @@
 # plugins
 cmd=rustrover
 if uname | grep -q "^MINGW"; then
-  cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/rustrover.cmd
+	cmd="$HOME"/AppData/Local/JetBrains/Toolbox/scripts/rustrover.cmd
 fi
 
 $cmd installPlugins \
-IdeaVIM \
-com.github.camork.fileExpander \
-com.joshestein.ideavim-quickscope \
-com.julienphalip.ideavim.functiontextobj \
-com.julienphalip.ideavim.peekaboo \
-eu.theblob42.idea.whichkey
+	IdeaVIM \
+	com.github.camork.fileExpander \
+	com.joshestein.ideavim-quickscope \
+	com.julienphalip.ideavim.functiontextobj \
+	com.julienphalip.ideavim.peekaboo \
+	eu.theblob42.idea.whichkey
